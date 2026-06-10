d7dbb7e392633071a761bc956a1155cb62a3fa52
osama <osamaalhamdan133@gmail.com>
2026-06-10 07:46:39 +0000

    Add 4-hidden_discovery.py

--- python-if_else_loops_functions/4-hidden_discovery.py
+++ python-if_else_loops_functions/4-hidden_discovery.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
-def main():
-# Retrieve and sort the attributes as required
-for name in sorted(dir(hidden_4)):
-# Filter out dunder methods
-if not name.startswith("__"):
-print(name)
-
-Ensure it runs only as a script
-if name == "main":
-main()
+if __name__ == "__main__":
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print(name)
